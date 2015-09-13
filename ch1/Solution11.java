9009442e9edff4c1df1d6e06a2c9accd58299fe2
Barry <Barry@128.237.200.88>
2015-09-13 17:05:40 -0400

    Solution11

--- ch1/Solution11.java
+++ ch1/Solution11.java
@@ -1,3 +1,29 @@
-public class Solution11 {
-
+public class Solution11 
+{
+	/**
+	 * This method will take a String input and test whether this String
+	 * has all unique characters.
+	 * 
+	 * @param input: the string that will be tested for unique letters
+	 * @return a boolean that shows whether the string tested has all unique characters
+	 */
+	public static boolean hasUnique(String input)
+	{
+		boolean hasUnique = true;
+        
+		
+		for (int i = 0; i < input.length()-1; i++)
+		{
+			for (int y = i + 1; y < input.length(); y++)
+			{
+                if (input.charAt(i) == input.charAt(y))
+                {
+                	//assign hasUnique to "false" when repetitive characters are found.
+				    hasUnique = false;
+				}   
+			}
+		}	
+        if (hasUnique == false) return false;
+        else return true;        
+	}  
 }
