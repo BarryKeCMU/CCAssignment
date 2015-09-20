326748371a48e58523e03552c54be5430c3f65cf
Barry <Barry@192.168.0.10>
2015-09-20 09:55:02 -0400

    Solution21

---
+++ ch2/Solution21.java
@@ -0,0 +1,32 @@
+public class Solution21
+{
+	/**
+	 * This method takes a linked list and deletes the duplicate elements inside
+	 * Note: I did not know Hashset, so I referred to the answers in the book and figured out the solution.
+	 * 
+	 * @param Node n: the first node in the linked list 
+	 */
+    public static void deleteDups(Node n)
+    {
+        Hashset <Integer> set = new Hashset<Integer>();
+        
+        Node previous = null;
+        
+        if (set.contains(n.data))
+        {
+            previous.next = n.next; //skip the current one    
+        }
+        
+        else 
+        {
+            set.add(n.data);
+            previous = n;
+        }
+        
+        n = n.next; //traverse through the linked list.
+        
+    }
+}
+
+
+
