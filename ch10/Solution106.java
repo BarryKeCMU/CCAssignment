f5a283166748ca1ff8f8e8928b26a6bc213b7927
Barry <Barry@128.237.139.4>
2015-10-04 08:54:22 -0400

    Solution106

---
+++ ch10/Solution106.java
@@ -0,0 +1,16 @@
+//Solution106.java
+
+/**
+*  Explains how to sort a 20 GB file with one string per line.
+*  
+*  Notes: I did not know the answer, so I checked the solution. I understood it and wrote it down by myself.
+*
+* @author Barry Ke
+* @version: Last modified on October 3, 2015
+* Find the kth to last element
+*/
+
+/*
+ * Answer: I will divide the 20 GB file into different chunks, and sort each chunk separately. Then I will merge all the sorted chunks into one 
+ * unit. 
+ */
