f2c26e48d28785bda357dd8beb898fa11fbeaba8
Barry <Barry@128.237.193.210>
2015-10-16 13:54:36 -0400

    Solution49

---
+++ ch4/Solution49.java
@@ -0,0 +1,12 @@
+//Solution49.java
+
+/**
+*  This class  
+*
+*  Notes: I did not know how to do it. I read the solution and understood the algorithm. I rewrote the codes by myself.
+*         I will definitely go back and review this question again in the future.
+*  
+* @author Barry Ke
+* @version: Last modified on October 15, 2015
+*/
+
