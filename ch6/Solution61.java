6551e50dc14ea3e99111051934357ec0235e028a
Barry <Barry@128.237.210.168>
2015-11-01 16:19:10 -0500

    Solution61

---
+++ ch6/Solution61.java
@@ -0,0 +1,17 @@
+//Solution61.java
+
+/**
+*  
+*  
+* @author Barry Ke
+* @version: Last modified on November 1, 2015
+*/
+
+
+/**
+*  Because we can only use the scale once, we have to treat each bottle differently. 
+*  Assume there are 20 bottles, bottle #1, bottle #2, bottle #3 and so on. Take one pill from bottle #1, two pills from bottle
+*  2, 3 pills from bottle 3, and so on. Weigh the mix of pills and get weight. 
+*  Use the formula: (weight - 210)/0.1, and we get the number of bottle that has 1.1 gram pills.
+* 
+*/
