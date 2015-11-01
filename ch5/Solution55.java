013248e757ebf5b2a47734d231cef06356d87e1a
Barry <Barry@192.168.0.10>
2015-11-01 09:20:53 -0500

    Solution55

---
+++ ch5/Solution55.java
@@ -0,0 +1,17 @@
+//Solution55.java
+
+/**
+*  
+*  
+*  
+*  
+* @author Barry Ke
+* @version: Last modified on November 11, 2015
+*/
+
+/**
+*  If n & (n-1) == 0, it means n and (n-1) cannot have 1s in common. This will happen when there is only 1 digit of 1 in n, and
+*  subtracting 1 from this number will make the condition n & (n-1) == 0 possible. Thus this equation is to check whether 
+*  n is a power of 2.
+*
+**/
