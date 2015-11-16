bb52b251e1ca70c846cddc20f6417ab5d54fee61
BarryKeCMU <lieweik@andrew.cmu.edu>
2015-11-16 15:19:13 -0500

    Solution69

---
+++ ch6/Solution69.java
@@ -0,0 +1,16 @@
+//Solution69.java
+//I refered to the solution in the book and figured out the answer.
+
+/**  
+* @author Barry Ke
+* @version: Last modified on November 16, 2015
+*/
+
+/**
+*   All the lockers are initially closed. In the first round, all the lockered are opened. In the second round, every second
+*   locker is closed. In the third round, every third locker is opened. Thus we could see that if the number of factors of 
+*   a locker's number is odd, this locker is left open after the 100th pass. Numbers with this feature are those called "perfect
+*   square," including 1, 4, 9, 16, 25, 36, 49, 64, 81, 100. Thus there are ten lockers that are open at the end of the
+*   process.    
+*  
+*/
