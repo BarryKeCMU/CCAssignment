961777f57606c12870768891dd3a7b1160285475
BarryKeCMU <lieweik@andrew.cmu.edu>
2015-11-15 21:20:47 -0500

    Solution65

---
+++ ch6/Solution65.java
@@ -0,0 +1,19 @@
+//Solution65.java
+//I refered to the solution in the book and figured out the answer.
+
+/**
+*  
+*  
+* @author Barry Ke
+* @version: Last modified on November 15, 2015
+*/
+
+/**
+*  5 Quart Jug               3 Quart Jug          
+*  5                         0           Note: Fill 5 quart jug with water
+*  2                         3                 Fill 3 qt jug with 5 qt jug's water
+*  2                         0                 Dump the water in 3 qt jug
+*  0                         2                 Fill 3 qt jug with 5 qt jug's water
+*  5                         2                 Fill 5 qt jug water
+*  4                         3                 Fill 3 qt jug wit 5 qt jug's water and we get a 4 qt jug of water
+*/
