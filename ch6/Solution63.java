de74dead256ef5f6debdbac630a93b891e3d2514
BarryKeCMU <lieweik@andrew.cmu.edu>
2015-11-15 20:14:44 -0500

    Solution63

---
+++ ch6/Solution63.java
@@ -0,0 +1,18 @@
+//Solution63.java
+
+/**
+*  
+*  
+* @author Barry Ke
+* @version: Last modified on November 15, 2015
+*/
+
+/**
+*  The two diagonally opposite corners have the same color. After we remove them, we are left with 30 whites and 32 blacks (or
+*  32 whites and 30 blacks). Each domino covers 2 squares, which means that in order to cover 32 blacks, 32 whites are needed. 
+*  But there are two less white squares than black squares. It is not gonna work. 
+*  
+*  
+* 
+*
+*/
